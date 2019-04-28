404fd91a11ede2157e8bf13b60fef6456e3c68eb
mgr-inz-rafal <rchabowski@gmail.com>
2019-04-28 14:11:15 +0200

    Paint gauchos in separate method

--- src/main.rs
+++ src/main.rs
@@ -40,22 +40,28 @@ fn game_logic(world: &mut WorldState) {
     world.x = world.x + 0.1;
 }
 
+fn paint_gauchos<G>(c: piston_window::Context, g: &mut G)
+where
+    G: piston_window::Graphics,
+{
+    let indices = gauchos::get_active_gauchos_indices();
+    indices
+        .iter()
+        .for_each(|&gi| match gauchos::get_gaucho_position(gi) {
+            Ok(pos) => rectangle(
+                [1.0, 0.0, 0.0, 1.0],
+                [pos[0], pos[1], 100.0, 100.0],
+                c.transform,
+                g,
+            ),
+            Err(msg) => (),
+        })
+}
+
 fn game_painter(wnd: &mut PistonWindow, e: Event, world: &WorldState) {
     wnd.draw_2d(&e, |c, g| {
         clear([1.0; 4], g);
-
-        let indices = gauchos::get_active_gauchos_indices();
-        indices
-            .iter()
-            .for_each(|&gi| match gauchos::get_gaucho_position(gi) {
-                Ok(pos) => rectangle(
-                    [1.0, 0.0, 0.0, 1.0],
-                    [pos[0], pos[1], 100.0, 100.0],
-                    c.transform,
-                    g,
-                ),
-                Err(msg) => (),
-            })
+        paint_gauchos(c, g);
     });
 }
 
@@ -67,7 +73,11 @@ fn main() {
 
     let world_state = WorldState { x: 200.0, y: 100.0 };
 
-    gauchos::add_gaucho();
+    let i = gauchos::add_gaucho();
+    let _ = gauchos::set_gaucho_position(i.unwrap(), [123.0, 70.0]);
+
+    let i = gauchos::add_gaucho();
+    let _ = gauchos::set_gaucho_position(i.unwrap(), [300.0, 150.0]);
 
     let window = create_window(&gui);
     game_loop(window, world_state, &game_logic, &game_painter);
