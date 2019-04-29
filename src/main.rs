33f20624855d44f3641fa52ba341b55809a2a640
mgr-inz-rafal <rchabowski@gmail.com>
2019-04-29 22:45:52 +0200

    Painting slots

--- src/main.rs
+++ src/main.rs
@@ -65,10 +65,34 @@ where
         })
 }
 
+fn paint_slots<G>(c: piston_window::Context, g: &mut G)
+where
+    G: piston_window::Graphics,
+{
+    let indices = gauchos::get_active_slots_indices();
+    indices
+        .iter()
+        .for_each(|&gi| match gauchos::get_slot_position(gi) {
+            Ok(pos) => rectangle(
+                [0.0, 1.0, 0.0, 1.0],
+                [
+                    pos[0] - GAUCHO_SIZE / 2.0,
+                    pos[1] - GAUCHO_SIZE / 2.0,
+                    GAUCHO_SIZE,
+                    GAUCHO_SIZE,
+                ],
+                c.transform,
+                g,
+            ),
+            Err(msg) => (),
+        })
+}
+
 fn game_painter(wnd: &mut PistonWindow, e: Event, world: &WorldState) {
     wnd.draw_2d(&e, |c, g| {
         clear([1.0; 4], g);
         paint_gauchos(c, g);
+        paint_slots(c, g);
     });
 }
 
@@ -81,10 +105,13 @@ fn main() {
     let world_state = WorldState { x: 200.0, y: 100.0 };
 
     let i = gauchos::add_gaucho();
-    let _ = gauchos::set_gaucho_position(i.unwrap(), [0.0, 0.0]);
-
-    let i = gauchos::add_gaucho();
     let _ = gauchos::set_gaucho_position(i.unwrap(), [300.0, 150.0]);
+
+    let s = gauchos::add_slot();
+    let s = gauchos::set_slot_position(s.unwrap(), [50.0, 50.0]);
+
+    let s = gauchos::add_slot();
+    let s = gauchos::set_slot_position(s.unwrap(), [250.0, 90.0]);
 
     let window = create_window(&gui);
     game_loop(window, world_state, &game_logic, &game_painter);
